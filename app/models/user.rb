8a1fad97ce3ffb91fc26b274f422f12d4ac71899
jaspreet <jaspreet.hora@gemsessence.com>
2018-11-09 20:19:48 +0530

    update views and navigations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stock, through: :user_stocks
 
+  def full_name
+    return " #{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymus"
+  end
+
   def stock_already_added?(ticker_symbol)
     stock = Stock.find_by_ticker?(ticker_symbol)
     return false unless stock
